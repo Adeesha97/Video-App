3e75dc37287c87baec2ee7279929a2685fd09677
Adeesha97 <adeeshakawshal1997@gmail.com>
2022-07-28 00:00:16 +0530

    add user routes

--- server/routes/User-Routes.js
+++ server/routes/User-Routes.js
@@ -1,6 +1,35 @@
 import express from "express";
-import {} from "../controllers/User-Controllers.js";
+import {
+  deleteUser,
+  dislike,
+  getUser,
+  like,
+  subscribe,
+  unsubscribe,
+  updateUser,
+} from "../controllers/User-Controllers.js";
 
 const router = express.Router();
+
+// UPDATE USER
+router.put("/:id", updateUser);
+
+// DELETE USER
+router.delete("/:id", deleteUser);
+
+// GET A USER
+router.get("/find/:id", getUser);
+
+// SUBSCRIBE A USER
+router.get("/sub/:id", subscribe);
+
+// UNSUBSCRIBE A USER
+router.get("/unsub/:id", unsubscribe);
+
+// LIKE A VIDEO
+router.get("/like/:videoId", like);
+
+// DISLIKE A VIDEO
+router.get("/dislike/:videoId", dislike);
 
 export default router;
